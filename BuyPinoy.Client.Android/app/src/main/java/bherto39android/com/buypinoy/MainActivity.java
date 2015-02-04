9dbe64f00b31eca1d784d621910558306fd1878b
bherto39 <bherto39android@gmail.com>
2015-02-04 23:30:38 +0800

    test and idea changes due to import

--- BuyPinoy.Client.Android/app/src/main/java/bherto39android/com/buypinoy/MainActivity.java
+++ BuyPinoy.Client.Android/app/src/main/java/bherto39android/com/buypinoy/MainActivity.java
@@ -28,12 +28,30 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        Test();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }//hsm
+    }
+
+    //TODO: fix this unresolved RestTemplate using gradle dependency
+    public void Test(){
+        //do nothing
+        // The connection URL
+        String url = "https://ajax.googleapis.com/ajax/" +
+                "services/search/web?v=1.0&q={query}";
+
+// Create a new RestTemplate instance
+        RestTemplate restTemplate = new RestTemplate();
+
+// Add the String message converter
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+// Make the HTTP GET request, marshaling the response to a String
+        String result = restTemplate.getForObject(url, String.class, "Android");
+
+    }
 }
